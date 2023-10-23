bdc5369678871d16cb5bc2e4c082a13b4a8498c4
amankbps <amankbps@gmail.com>
2023-10-24 00:06:53 +0530

    Pusing codes

---
+++ app.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+    "h1",
+    { id: "heading1",
+      style:{color:"green"}
+    },
+    "this is Heading one"
+  );
+const heading2 = React.createElement(
+    "h1",
+    { id: "title",
+      className:"hading",
+ },
+    "this is React"
+  );
+const conatiner = React.createElement(
+    "h1",
+    { id: "container" },
+    [heading1,heading2]
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(conatiner);
